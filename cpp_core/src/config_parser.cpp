62b368a7608af97e670a0662658ca018b3d7bec7
rollcat-endl <lighthouse@mail.ustc.edu.cn>
2025-12-07 20:28:44 +0800

    实现了哈希表方法的配置文件解析器

--- cpp_core/src/config_parser.cpp
+++ cpp_core/src/config_parser.cpp
@@ -12,6 +12,10 @@
 ConfigParser::ConfigParser(const std::string &config_file)
     : config_file_path_(config_file)
 {
+    i_config_map = {
+        {"LIVE_MIN", 0}, {"LIVE_MAX", 1}, {"BREED_MIN", 2}, {"BREED_MAX", 3}, {"VISION", 4}, {"X", 5}, {"Y", 6}};
+    f_config_map = {
+        {"DEATH_RATE", 0}, {"ENERGY_COMSUMPTION", 1}, {"RESTORE_PROB", 2}, {"RESTORE_VALUE", 3}};
 }
 
 bool ConfigParser::loadConfig()
@@ -41,23 +45,20 @@ bool ConfigParser::loadConfig()
         {
             std::string key = line.substr(0, pos);
             std::string value = line.substr(pos + 1);
-            if (lineNumber < 5)
+            // 去除前后空格
+            key.erase(0, key.find_first_not_of(" \t"));
+            key.erase(key.find_last_not_of(" \t") + 1);
+            value.erase(0, value.find_first_not_of(" \t"));
+            value.erase(value.find_last_not_of(" \t") + 1);
+            // 存储配置
+            if (i_config_map.find(key) != i_config_map.end())
             {
-                i_config[lineNumber] = std::stoi(value);
+                i_config[i_config_map[key]] = std::stoi(value);
             }
-            else if (lineNumber < 9)
+            else if (f_config_map.find(key) != f_config_map.end())
             {
-                f_config[lineNumber - 5] = std::stod(value);
+                f_config[f_config_map[key]] = std::stod(value);
             }
-            else if (lineNumber == 9)
-            {
-                i_config[5] = std::stoi(value);
-            }
-            else if (lineNumber == 10)
-            {
-                i_config[6] = std::stoi(value);
-            }
-            lineNumber++;
         }
     }
     return true;
@@ -65,33 +66,9 @@ bool ConfigParser::loadConfig()
 int ConfigParser::getInt(const std::string &key, int default_value) const
 {
     // 获取类型为整型的配置值
-    if (key == "Live_min")
+    if (i_config_map.find(key) != i_config_map.end())
     {
-        return i_config[0];
-    }
-    else if (key == "Live_max")
-    {
-        return i_config[1];
-    }
-    else if (key == "Breed_min")
-    {
-        return i_config[2];
-    }
-    else if (key == "Breed_max")
-    {
-        return i_config[3];
-    }
-    else if (key == "Vision")
-    {
-        return i_config[4];
-    }
-    else if (key == "X")
-    {
-        return i_config[5];
-    }
-    else if (key == "Y")
-    {
-        return i_config[6];
+        return i_config[i_config_map.at(key)];
     }
     else
     {
@@ -102,21 +79,9 @@ int ConfigParser::getInt(const std::string &key, int default_value) const
 double ConfigParser::getDouble(const std::string &key, double default_value) const
 {
     // 获取类型为浮点型的配置值
-    if (key == "Death_Rate")
+    if (f_config_map.find(key) != f_config_map.end())
     {
-        return f_config[0];
-    }
-    else if (key == "Energy_comsumption")
-    {
-        return f_config[1];
-    }
-    else if (key == "Restore_prob")
-    {
-        return f_config[2];
-    }
-    else if (key == "Restore_value")
-    {
-        return f_config[3];
+        return f_config[f_config_map.at(key)];
     }
     else
     {
@@ -126,86 +91,39 @@ double ConfigParser::getDouble(const std::string &key, double default_value) con
 
 void ConfigParser::setInt(const std::string &key, int value)
 {
-    // 保存配置到文件
-    if (key == "Live_min")
+    // 设置类型为整型的配置值
+    if (i_config_map.find(key) != i_config_map.end())
     {
-        i_config[0] = value;
-    }
-    else if (key == "Live_max")
-    {
-        i_config[1] = value;
-    }
-    else if (key == "Breed_min")
-    {
-        i_config[2] = value;
-    }
-    else if (key == "Breed_max")
-    {
-        i_config[3] = value;
-    }
-    else if (key == "Vision")
-    {
-        i_config[4] = value;
-    }
-    else if (key == "X")
-    {
-        i_config[5] = value;
-    }
-    else if (key == "Y")
-    {
-        i_config[6] = value;
+        i_config[i_config_map.at(key)] = value;
     }
 }
 
 void ConfigParser::setDouble(const std::string &key, double value)
 {
     // 设置类型为浮点型的配置值
-    if (key == "Death_Rate")
+    if (f_config_map.find(key) != f_config_map.end())
     {
-        f_config[0] = value;
-    }
-    else if (key == "Energy_comsumption")
-    {
-        f_config[1] = value;
-    }
-    else if (key == "Restore_prob")
-    {
-        f_config[2] = value;
-    }
-    else if (key == "Restore_value")
-    {
-        f_config[3] = value;
+        f_config[f_config_map.at(key)] = value;
     }
 }
 
 bool ConfigParser::hasKey(const std::string &key) const
 {
     // 检查配置键值对是否存在
-    if (key == "Live_min" || key == "Live_max" || key == "Breed_min" || key == "Breed_max" || key == "Vision" || key == "X" || key == "Y" ||
-        key == "Death_Rate" || key == "Energy_comsumption" || key == "Restore_prob" || key == "Restore_value")
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return (i_config_map.find(key) != i_config_map.end() || f_config_map.find(key) != f_config_map.end());
 }
 
 void ConfigParser::printAll() const
 {
     // 输出所有配置值
-    std::cout << "Minimum number of neighbors for a cell to survive is :" << i_config[0] << std::endl;
-    std::cout << "Maximum number of neighbors for a cell to survive is :" << i_config[1] << std::endl;
-    std::cout << "Minimum number of neighbors for a cell to be born is :" << i_config[2] << std::endl;
-    std::cout << "Maximum number of neighbors for a cell to be born is :" << i_config[3] << std::endl;
-    std::cout << "Cell vision distance is :" << i_config[4] << std::endl;
-    std::cout << "Cell death probability is :" << f_config[0] << std::endl;
-    std::cout << "Cell energy consumption is :" << f_config[1] << std::endl;
-    std::cout << "Cell energy restore probability is :" << f_config[2] << std::endl;
-    std::cout << "Cell energy restore value is :" << f_config[3] << std::endl;
-    std::cout << "width of the gird is :" << i_config[5] << std::endl;
-    std::cout << "height of the gird is :" << i_config[6] << std::endl;
+    std::cout << "Integer Configurations:" << std::endl;
+    for (const auto &pair : i_config_map)
+    {
+        std::cout << pair.first << " = " << i_config[pair.second] << std::endl;
+    }
+    std::cout << "Double Configurations:" << std::endl;
+    for (const auto &pair : f_config_map)
+    {
+        std::cout << pair.first << " = " << f_config[pair.second] << std::endl;
+    }
 }
-// TODO:哈希表
-// TODO:字典
